f2848cbdf0d35315b8caee6855675338cd14d7de
ezeev <evan.pease@gmail.com>
2018-05-10 12:59:12 -0700

    more search tests

--- bestbuy/bestbuy_test.go
+++ bestbuy/bestbuy_test.go
@@ -5,12 +5,27 @@ import (
 	"testing"
 	"time"
 
+	"gopkg.in/couchbase/gocb.v1"
+
 	"github.com/ezeev/go-couchbase-examples/bestbuy"
 )
 
+func mustOpenBucket(name string) *gocb.Bucket {
+	cluster, err := bestbuy.CbConnect("couchbase://localhost", "evan", "password")
+	if err != nil {
+		panic(err)
+	}
+
+	bucket, err := bestbuy.CbOpenBucket(name, cluster)
+	if err != nil {
+		panic(err)
+	}
+	return bucket
+}
+
 func TestGetProduct(t *testing.T) {
 
-	cluster, err := bestbuy.CbConnect("couchbase://localhost", "evan", "password")
+	/*cluster, err := bestbuy.CbConnect("couchbase://localhost", "evan", "password")
 	if err != nil {
 		t.Error(err)
 	}
@@ -18,47 +33,61 @@ func TestGetProduct(t *testing.T) {
 	bucket, err := bestbuy.CbOpenBucket("bb-catalog", cluster)
 	if err != nil {
 		t.Error(err)
-	}
+	}*/
+	bucket := mustOpenBucket("bb-catalog")
 
 	prod1, err := bestbuy.GetProduct("1184298", bucket)
+	if err != nil {
+		t.Error(err)
+	}
 	t.Logf("Type attribute of doc returned: %s", prod1.Type)
 	if prod1.Type != "Game" {
 		t.Error("Type != \"Game\"")
 	}
 
-	// FTS
-	res, err := bestbuy.Search("bb-search", "gears of war", bucket)
+}
+
+func TestBoostSearch(t *testing.T) {
+	bucket := mustOpenBucket("bb-catalog")
+	res, err := bestbuy.Search("bb-search", "xbox sku:\"3650503\"^1", bucket)
 	if err != nil {
 		t.Error(err)
 	}
 
 	bucket.Close()
-	cluster.Close()
 
+	if res.Hits()[0].Id != "3650503" {
+		t.Errorf("Expected id 3650503 to be in position. Got %s instead\n", res.Hits()[0])
+	}
+
+	fmt.Printf("\tFound %d results in %s\n", res.TotalHits(), res.Took().String())
 	for _, hit := range res.Hits() {
-		fmt.Printf("%s\n", hit.Id)
+		fmt.Printf("\t%s : %f\n", hit.Id, hit.Score)
 	}
 	// facets
 	for _, f := range res.Facets() {
 		fmt.Println(f.Field)
-		for k, v := range f.Terms {
-			fmt.Printf("\t%s : %s\n", k, v)
+		for _, v := range f.Terms {
+			fmt.Printf("\t%s : %d\n", v.Term, v.Count)
 		}
 	}
 
 }
 
+func TestFilteredSearch(t *testing.T) {
+	bucket := mustOpenBucket("bb-catalog")
+	res, err := bestbuy.Search("bb-search", "red dead +platform:\"PS3 Games\"", bucket)
+	if err != nil {
+		t.Error(err)
+	}
+	for i, hit := range res.Hits() {
+		fmt.Printf("\tHit %d platform: %s\n", i, hit.Fields)
+	}
+}
+
 func TestCounters(t *testing.T) {
 
-	cluster, err := bestbuy.CbConnect("couchbase://localhost", "evan", "password")
-	if err != nil {
-		t.Error(err)
-	}
-
-	bucket, err := bestbuy.CbOpenBucket("tests", cluster)
-	if err != nil {
-		t.Error(err)
-	}
+	bucket := mustOpenBucket("test")
 
 	// use KV
 	rows := 10
--- bestbuy/search.go
+++ bestbuy/search.go
@@ -6,9 +6,12 @@ import (
 )
 
 func Search(index string, q string, bucket *gocb.Bucket) (gocb.SearchResults, error) {
-	query := gocb.NewSearchQuery(index, cbft.NewMatchQuery(q))
-	query.AddFacet("manufacturer", cbft.NewTermFacet("manufacturer", 5))
-	query.AddFacet("platform", cbft.NewTermFacet("platform", 5))
+	//query := gocb.NewSearchQuery(index, cbft.NewMatchQuery(q))
+	query := gocb.NewSearchQuery(index, cbft.NewQueryStringQuery(q))
+
+	query.AddFacet("manufacturer", cbft.NewTermFacet("manufacturer", 10))
+	query.AddFacet("platform", cbft.NewTermFacet("platform", 10))
+	query.Fields("platform")
 	res, err := bucket.ExecuteSearchQuery(query)
 	if err != nil {
 		return nil, err
